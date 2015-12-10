28cd23bc1c0a4719d414e4518811b6e2b17b8ab9
Laurent Claessens <moky.math@gmail.com>
2015-12-10 15:27:04 +0100

    Making the iterator in HashTable. Still under development.

--- HashTable.h
+++ HashTable.h
@@ -26,26 +26,98 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // The keys are ordered in the order of declaration.
 // One get the value associated with 'x' by my_hash_table[x].
 // We add at the end and an iterator begins at the beginning.
+// When iterating, we get the values (not the keys);
 template <class K,class V>
 class HashTable
 {
-    class node
-    {
-        K key;
-        V value;
-        node* next;
-    };
+    private:
+        class node
+        {
+            private:
+                node* next;
+            public:
+                const K key;
+                V value;
+                node(K,V);
+        };
 
-    private :
         node* first;
-        node* lase;
+        node* last;     // this is the last one, not the 'past-the-last'
     public:
+
+        class iterator
+        {
+            private:
+                node* punt;
+            public:
+                iterator();
+                iterator(node*);
+                bool operator==(iterator itr) const;
+                bool operator!=(iterator itr) const;
+                iterator operator++(int);       //i++
+                iterator& operator++();         //++i
+        };
         HashTable();
-        setValue(K ,V );
+        void setValue(K ,V );
+        bool isKey(K) const;     // return true if the given key exists.
+        iterator begin() const;
+        iterator end() const;
 };
 
 template <class K,class V>
-HashTable<K,V>::HashTable(): first(0)  {}
-HashTable<K,V>::setValue(K jey,V value)
+HashTable<K,V>::HashTable(): first(0),last(0)  {}
+template <class K,class V>
+void HashTable<K,V>::setValue(const K key,V value) {   }
+template <class K,class V>
+HashTable<K,V>::node::node(const K k,V v): next(0),key(k),value(v)  { }
+
+template <class K,class V>
+HashTable<K,V>::iterator::iterator():punt(0){}
+template <class K,class V>
+HashTable<K,V>::iterator::iterator(HashTable<K,V>::node* n):punt(n){}
+
+template <class K,class V>
+typename HashTable<K,V>::iterator HashTable<K,V>::begin() const 
 {
+    return iterator(first);
+}
+template <class K,class V>
+typename HashTable<K,V>::iterator HashTable<K,V>::end() const  
+{
+    return iterator(last.next);
+}
+
+template <class K,class V>
+typename HashTable<K,V>::iterator& HashTable<K,V>::iterator::operator++()
+{
+    if (punt) punt=punt->next;       // if the iterator points to 0, making "itr++" gives again an itarator pointing to 0.
+    return *this;
+}
+template <class K,class V>
+typename HashTable<K,V>::iterator HashTable<K,V>::iterator::operator++(int)
+{
+    iterator aux=*this;
+    ++*this;
+    return aux;
+}
+
+template <class K,class V>
+bool HashTable<K,V>::iterator::operator==(const HashTable<K,V>::iterator itr) const
+{
+    return punt == itr.punt;
+}
+template <class K,class V>
+bool HashTable<K,V>::iterator::operator!=(const HashTable<K,V>::iterator itr) const
+{
+    return !(*this==itr);
+}
+template <class K,class V>
+V HashTable<K,V>::operator[](const K) const
+{
+    cout<<"Still to be done";
+}
+template <class K,class V>
+V HashTable<K,V>::operator[](HashTable<K,V>::iterator itr ) const
+{
+    return itr->value;
 }
--- tests/HashTableTest.cpp
+++ tests/HashTableTest.cpp
@@ -6,6 +6,22 @@ using namespace std;
 
 int main()
 {
-    HashTable<int,string> my_hash();
-    my_hash.setValue(4,"bonjour");
+    HashTable<int,string> my_hash;
+    my_hash.setValue(4,"hello 4");
+    my_hash.setValue(5,"hello 5");
+    my_hash.setValue(1,"hello 1");
+    my_hash.setValue(4,"Should not work");
+
+    string s1="";
+    string s2="";
+    for (HashTable<int,string>::iterator itr=my_hash.begin();itr!=my_hash.end();itr++) {
+        s1=s1+my_hash[itr]; 
+    }
+
+    assert(s1=="hello 4hello 5hello 1");
+    for (HashTable<int,string>::iterator itr=my_hash.begin();itr!=my_hash.end();++itr) {s2=s2+my_hash[itr]; }
+    assert(s1==s2);
+
+    assert(my_hash[1]=="hello 1");
+    assert(my_hash[5]=="hello 5");
 }
